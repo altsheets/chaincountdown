769137168129ac98efc1c6c33472f9a12504ac5e
altsheets <altsheets+coding@gmail.com>
2015-09-01 20:08:08 +0200

    run.sh to start in docker container CMD

---
+++ docker/run.sh
@@ -0,0 +1,13 @@
+# As docker can only execute one CMD 
+# (and when I try to trick it with 'sh -c ...' the CTRL-C stopped working)
+# this file gets executed at each start of the docker container
+
+# remove if exists
+FOLDER="chaincountdown"
+[-d $FOLDER] && rm -r $FOLDER
+
+# clone newest version from github  
+/usr/bin/git clone https://github.com/altsheets/chaincountdown
+
+# start server
+/usr/bin/python chaincountdown/server.py
