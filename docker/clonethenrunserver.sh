b16fbd66c674729bfe7ff1ad14f6c02822438dcf
altsheets <altsheets+coding@gmail.com>
2015-09-01 23:21:29 +0200

    pull instead of rm clone

--- docker/clonethenrunserver.sh
+++ docker/clonethenrunserver.sh
@@ -1,22 +1,27 @@
 # As docker can only execute one CMD 
-# and when I try to trick it with 'sh -c ...' the CTRL-C stopped working
-# this file gets executed at each start of the docker container
+# this whole script gets executed at each start of the docker container
 
 echo 
 echo CMD clonethenrunserver.sh
 echo 
+
 FOLDER="chaincountdown"
 if [ -d $FOLDER ];
 then
-   rm -r $FOLDER
-   echo Removed old source folder.
+   cd $FOLDER
+   echo Updating to newest source code from GitHub:
+   /usr/bin/git pull
+else
+   mkdir $FOLDER
+   cd $FOLDER
+   echo Cloning newest source code from GitHub:
+   /usr/bin/git clone https://github.com/altsheets/chaincountdown
 fi
 
-echo Cloning newest source code from GitHub:
-/usr/bin/git clone https://github.com/altsheets/chaincountdown
-
+echo
 echo Starting Server:
 echo If you have started the container with -t -i ... press CTRL-C to stop. 
 echo If not ... then you have to kill it with 'killall -9 docker'.
-echo 
+echo
+ 
 /usr/bin/python chaincountdown/server.py
