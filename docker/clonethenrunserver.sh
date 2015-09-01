1efe93963f6d768c601371af2fa1cc1ae9c7cf08
altsheets <altsheets+coding@gmail.com>
2015-09-01 21:49:50 +0200

    more printout infos

--- docker/clonethenrunserver.sh
+++ docker/clonethenrunserver.sh
@@ -2,14 +2,19 @@
 # and when I try to trick it with 'sh -c ...' the CTRL-C stopped working
 # this file gets executed at each start of the docker container
 
-echo Remove folder if exists:
-FOLDER="chaincountdown"
-[ -d "$FOLDER" ] && rm -r $FOLDER
 
-echo Clone from Github:
+FOLDER="chaincountdown"
+if [ -d $FOLDER ];
+then
+   rm -r $FOLDER
+   echo Removed old source folder.
+fi
+
+echo Cloning newest source code from GitHub:
 /usr/bin/git clone https://github.com/altsheets/chaincountdown
 
 echo Starting Server:
 echo If you have started the container with -t .. just press CTRL-C to stop. 
 echo If not .. then you have to kill it with 'killall -9 docker'.
+echo 
 /usr/bin/python chaincountdown/server.py
