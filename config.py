defb8de8535c319c2dfaa77fc48b271765fcc50e
altsheets <altsheets+coding@gmail.com>
2016-01-05 19:44:48 +0100

    v13.3 NXT new average of blocktimes

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ SERVER ={
          # "nhz":  "http://localhost:7776/nhz",
          }
 
-BLOCKSPERMINUTE = {"nxt": 1 / 1.9005,
+BLOCKSPERMINUTE = {"nxt": 1 / 1.88,     # measured at block height 607838 (see nxtforum post)
                    "nhz": 1 / 1.74003}  # measured new between block 478988 and 488988 -> timestamp 50043922 to 51087942
 GENESIS = {"nhz": 1395526942.22, 
            "nxt": 1385294400}
