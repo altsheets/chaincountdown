d6d8fa9e2982c29abe25d4dd67b6e30297ba4a82
altsheets <altsheets+coding@gmail.com>
2016-01-24 14:47:29 +0100

    testing port 7774

---
+++ tools/test7774.py
@@ -0,0 +1,61 @@
+
+"""
+
+curl -4 -m5 --data '{"platform":"TEST","protocol":1,"application":"TEST Bot","requestType":"getInfo","version":"v0.1"}' "http://woll-e.net:7774/nhz"
+
+is the example.
+
+But I just cannot get it working, however I try.
+
+Always the same answer:
+
+http://woll-e.net:7774/nhz
+test 1:
+{"cause":"Unexpected character (r) at position 0.","error":"Your peer is blacklisted"}
+test 2:
+{"cause":"Unexpected character (r) at position 0.","error":"Your peer is blacklisted"}
+
+Please help. Thanks.
+
+"""
+ 
+import urllib2
+
+NODE="eu6.woll-e.net"
+NODE="woll-e.net"
+
+PORT=7774
+URL="http://%s:%s/nhz" % (NODE, PORT)
+
+print URL
+
+def test1():
+  print "test 1:"
+  data='{"platform":"TEST","protocol":1,"application":"TEST Bot","requestType":"getInfo","version":"v0.1"}'
+  s=urllib2.urlopen(url=URL, data=data)
+  answer=s.read()
+  s.close()
+  print answer
+  
+def test2():
+  print "test 2:"
+  try:
+    import requests
+  except:
+    print "pip install requests"
+    return False
+  
+  data={"platform":"TEST","protocol":1,"application":"TEST Bot","requestType":"getInfo","version":"v0.1"}
+  r=requests.post(URL, data=data)
+  print r.text
+  
+
+
+
+  
+if __name__=="__main__":
+  
+  test1()
+  test2()
+  
+  
